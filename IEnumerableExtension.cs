e0719b0f57875333a828914172250d59ad57858f
HChristopherNaoyuki <st10462415@rcconnect.edu.za>
2025-08-22 15:11:26 +0200

    Add project files.

---
+++ IEnumerableExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_intervals_demo_cs
+{
+    public static class IEnumerableExtension
+    {
+        public static IEnumerable<T> NewWhere<T>(
+            this IEnumerable<T> items, Func<T, bool> predicate)
+        {
+            var list = new List<T>();
+
+            foreach(var item in items)
+            {
+                if(predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_intervals_demo_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            var even_items = items.NewWhere(x => x % 2 == 0);
+
+            foreach(var item in even_items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
